48be6084aca69a6a9ff206feb7b1af5e965d795d
sachio222 <sachio222@users.noreply.github.com>
2020-04-01 23:38:00 -0400

    commit

---
+++ model_tests/ec_ca3.py
@@ -0,0 +1,5 @@
+import torch
+from model import modules
+
+if __name__ == '__main__':
+    print(f'The name of this test is {__name__}')
--- unit_tests/ec_ca3.py
+++
@@ -1,8 +0,0 @@
-import sys
-import torch
-
-print(sys.path)
-from aha5.model import modules
-
-if __name__ == '__main__':
-    print(f"name is {__name__}")
